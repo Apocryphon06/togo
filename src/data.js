6e919663ef9819f0439c3691a9a47ce1ee153e7b
Apocryphon06 <hrithikchandrashekar@gmail.com>
2022-09-22 11:28:05 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const sliderArray = [
+  {
+    title: "WINTER SALE",
+    description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVAL",
+    bg: "f5fafd",
+  },
+  {
+    title: "WINTER SALE",
+    description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVAL",
+    bg: "fcf1ed",
+  },
+  {
+    title: "WINTER SALE",
+    description: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVAL",
+    bg: "dfdfdf",
+  },
+];
